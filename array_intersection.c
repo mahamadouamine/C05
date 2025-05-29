e80659a9dbb3ad3ef99b733c00ac55c603d932d3
Mahamadou Amine <mahamadouamine@iMac-de-Amine.local>
2025-05-29 20:50:27 +0100

    4commit

--- array_intersection.c
+++ array_intersection.c
@@ -1,32 +1,34 @@
-#include <stdio.h>
+#include <stdio.h> // Inclusion de la bibliothèque standard d'entrée/sortie pour utiliser printf et scanf
 
-int deja_la(int tab[], int taille, int val) {
-    for (int i = 0; i < taille; i++) {
-        if (tab[i] == val) return 1;
-    }
-    return 0;
-}
+int main() { // Déclaration de la fonction principale, point d'entrée du programme
+    int n1, n2, t1[100], t2[100], inter[100], taille = 0; 
+    
 
-int main() {
-    int n1, n2, t1[100], t2[100], inter[100], taille = 0;
+    int tab[100]; // Déclaration d’un tableau tab (inutile ici)
+    int val; // Déclaration d’une variable val (inutile ici)
 
-    scanf("%d", &n1);
-    for (int i = 0; i < n1; i++) scanf("%d", &t1[i]);
+    scanf("%d", &n1); // Lecture du nombre d'éléments dans le tableau t1
+    for (int i = 0; i < n1; i++) scanf("%d", &t1[i]); 
+    // Lecture des n1 éléments dans le tableau t1
 
-    scanf("%d", &n2);
-    for (int i = 0; i < n2; i++) scanf("%d", &t2[i]);
+    scanf("%d", &n2); // Lecture du nombre d'éléments dans le tableau t2
+    for (int i = 0; i < n2; i++) scanf("%d", &t2[i]); 
+    // Lecture des n2 éléments dans le tableau t2
 
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < n2; j++) {
-            if (t1[i] == t2[j] && !deja_la(inter, taille, t1[i])) {
-                inter[taille++] = t1[i];
+    for (int i = 0; i < n1; i++) { // Parcours de tous les éléments de t1
+        for (int j = 0; j < n2; j++) { // Parcours de tous les éléments de t2
+            if (t1[i] == t2[j]) { // Si un élément de t1 est égal à un élément de t2
+                inter[taille++] = t1[i]; // Ajouter l'élément à l'intersection et incrémenter taille
             }
         }
     }
+    
 
-    printf("Intersection : ");
-    for (int i = 0; i < taille; i++) printf("%d ", inter[i]);
-    printf("\n");
+    printf("Intersection : "); // Affiche le texte "Intersection : "
+    for (int i = 0; i < taille; i++) printf("%d ", inter[i]); 
+    // Affiche chaque élément du tableau inter (séparé par un espace)
+    printf("\n"); // Saut de ligne après l'affichage
 
-    return 0;
+    return 0; // Fin du programme avec un code de retour 0 (succès)
 }
+
