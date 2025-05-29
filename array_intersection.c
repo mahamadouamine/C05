56924ab255750abdc5bbf613523321bfae9a13b3
Mahamadou Amine <mahamadouamine@iMac-de-Amine.local>
2025-05-29 22:15:20 +0100

    4 commit

--- array_intersection.c
+++ array_intersection.c
@@ -1,11 +1,9 @@
 #include <stdio.h> // Inclusion de la bibliothèque standard d'entrée/sortie pour utiliser printf et scanf
 
 int main() { // Déclaration de la fonction principale, point d'entrée du programme
-    int n1, n2, t1[100], t2[100], inter[100], taille = 0; 
-    
-
-    int tab[100]; // Déclaration d’un tableau tab (inutile ici)
-   
+    int taille = 0;
+    int n1, n2;
+    int t1[100], t2[100], inter[100]; // Utiliser une taille fixe ici
 
     scanf("%d", &n1); // Lecture du nombre d'éléments dans le tableau t1
     for (int i = 0; i < n1; i++) scanf("%d", &t1[i]); 
@@ -19,11 +17,10 @@ int main() { // Déclaration de la fonction principale, point d'entrée du progr
         for (int j = 0; j < n2; j++) { // Parcours de tous les éléments de t2
             if (t1[i] == t2[j]) { // Si un élément de t1 est égal à un élément de t2
                 inter[taille++] = t1[i]; // Ajouter l'élément à l'intersection et incrémenter taille
+               
             }
         }
     }
-    
-
     printf("Intersection : "); // Affiche le texte "Intersection : "
     for (int i = 0; i < taille; i++) printf("%d ", inter[i]); 
     // Affiche chaque élément du tableau inter (séparé par un espace)
